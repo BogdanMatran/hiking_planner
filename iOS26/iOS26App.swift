25a22591f703787fbe9ffba7b1b8bebfd44649a9
Bogdan Matran <bogdan.matran@ti8m.ch>
2025-06-12 13:45:05 +0200

    Initial Commit

---
+++ iOS26/iOS26App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS26App.swift
+//  iOS26
+//
+//  Created by Matran Bogdan on 12.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct iOS26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
